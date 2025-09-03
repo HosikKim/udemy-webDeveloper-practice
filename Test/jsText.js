ca5089a2bbfe1ae7d1e370a79ab43770b19310b4
HosikKim <ghtlr5493@naver.com>
2025-09-03 11:55:37 +0900

    Html 기초 complete

---
+++ Test/jsText.js
@@ -0,0 +1,19 @@
+// find elements
+var banner = $("#banner-message")
+var button = $("button")
+
+// handle click and add class
+button.on("click", () => {
+    {
+        {
+            {
+                {
+                    {
+                        
+                    }
+                }
+            }
+        }
+    }
+  banner.toggleClass("alt")
+})
