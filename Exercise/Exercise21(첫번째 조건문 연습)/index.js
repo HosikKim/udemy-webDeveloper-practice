cb37c7bf325ce6ac77abccc6f2a7d6b304aba4f8
HosikKim <ghtlr5493@naver.com>
2025-12-22 15:24:16 +0900

    feat: [Exercise] add Exercise21(첫번째 조건문 연습) in section16_JavaScript판단내리기

---
+++ Exercise/Exercise21(첫번째 조건문 연습)/index.js
@@ -0,0 +1,10 @@
+function isEven(num) {
+    //num의 값이 짝수면 even 출력
+    //홀수면 아무 반응 X
+
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+    if (num % 2 == 0) {
+        console.log('even')
+    }
+    //AND THIS LINE ↑↑↑↑↑
+}
