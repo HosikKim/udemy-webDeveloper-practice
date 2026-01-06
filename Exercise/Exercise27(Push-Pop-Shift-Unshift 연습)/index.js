2096768a164867f626e918bfc757d8761d67063c
HosikKim <ghtlr5493@naver.com>
2026-01-06 15:42:44 +0900

    feat: [Exercise] add Exercise27(Push-Pop-Shift-Unshift 연습) in section17_JavaScript배열

---
+++ Exercise/Exercise27(Push-Pop-Shift-Unshift 연습)/index.js
@@ -0,0 +1,12 @@
+const planets = ['The Moon', 'Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+
+// YOUR CODE GOES BELOW THIS LINE:
+planets.shift();
+planets.push("Saturn");
+planets.unshift("Mercury");
+
+
+// Push - 배열 맨 끝 요소 추가
+// Pop - 배열 맨 끝 요소 제거
+// Shift - 배열 첫 요소 제거
+// Unshift - 배열 첫 요소 추가
